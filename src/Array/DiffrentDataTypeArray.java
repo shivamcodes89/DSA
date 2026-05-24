80fac1605a2b04b3595474177a13b699be08cfd3
Shivam Rajput <rajputshivam8550855@gmail.com>
2026-05-24 19:58:19 +0530

    dataType

---
+++ src/Array/DiffrentDataTypeArray.java
@@ -0,0 +1,18 @@
+package Array;
+
+import java.util.Arrays;
+
+public class DiffrentDataTypeArray {
+    public static void main(String[] args) {
+//        String[] arr = new String[5];
+//        System.out.println(Arrays.toString(arr));
+        String[] arr = {"ajeet", "abhay", "sumit"};
+        System.out.println(Arrays.toString(arr));
+
+        char[] ch = {'a', 'b', 'c', 'd'};
+        System.out.println(Arrays.toString(ch));
+
+        double[] du = {5.3, 3.5, 9.9};
+        System.out.println(Arrays.toString(du));
+    }
+}
